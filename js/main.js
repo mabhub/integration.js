4edbfb24e850c9a8528d2ff65289424fcc89139c
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2017-01-20 18:09:07 +0100

    Do first quick and dirty proof of concept

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
-console.log('ok');
+const request = new XMLHttpRequest();
+
+request.onreadystatechange = function() {
+    if (this.readyState === 4) {
+        if (this.status >= 200 && this.status < 400) {
+            let $newDom = $(this.responseText).find('.content-wrapper');
+
+            $('a', $newDom).each((index, link) => {
+                let href = link.getAttribute('href');
+
+                if (href[0] === '/') {
+                    link.setAttribute('href', '?sip=' + href);
+                }
+
+            });
+            $newDom.appendTo('#integration_infofficiel');
+        } else {
+            // Error :(
+        }
+    }
+};
+
+let source = '/sip/';
+
+request.open('GET', 'https://nameless-depths-66550.herokuapp.com' + source, true);
+request.send();
